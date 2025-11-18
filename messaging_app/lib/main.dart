d978360d2ae0216a834763285dd35008d0d670ee
Samuel Berhan <berhansami10@gmail.com>
2025-11-18 15:33:24 -0500

    commit

--- messaging_app/lib/main.dart
+++ messaging_app/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 // App imports
 import 'app/app_router.dart';
 import 'app/theme.dart';
 import 'services/auth_service.dart';
-import 'screens/splash/splash_screen.dart';
+import 'screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MessageBoardApp());
 }
 
